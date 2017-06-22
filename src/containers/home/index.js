48b3a40de694a1fe5b46e52c81a4a242ffe78fa9
Shawn Petros <arashcuzi@gmail.com>
2017-06-22 12:40:19 -0500

    more stuffs...

---
+++ src/containers/home/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+} from '../../modules/counter'
+import { postAnything } from '../../modules/postAnything';
+import Clock from '../clock'
+
+const Home = props => (
+  <div>
+    <Clock />
+
+    <h1>Home</h1>
+    <p>Count: {props.count}</p>
+
+    <p>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+    </p>
+
+    <p>
+      <button onClick={() => props.postAnything('hello world!')}>Post Anything</button>
+    </p>
+
+    <p>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
+      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+    </p>
+
+    <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
+  </div>
+)
+
+const mapStateToProps = state => ({
+  count: state.counter.count,
+  isIncrementing: state.counter.isIncrementing,
+  isDecrementing: state.counter.isDecrementing
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
+  postAnything
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './containers/app'
+import { makeClock } from './containers/clock';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import 'sanitize.css/sanitize.css'
+import './index.css'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const target = document.querySelector('#root')
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  target
+)
+
+setTimeout(() => {
+  document.getElementById('bangalore');
+  new makeClock('bangalore');
+  new makeClock('milwaukee');
+  new makeClock('new_york');
+}, 1000)
