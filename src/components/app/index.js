bd1c01e9e5256aa58a99d4117673ce2f2bb6bd04
Jesse Schmucker <jesselee34@gmail.com>
2017-06-23 12:42:44 -0500

    Started making sexy

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import Clock from '../clock';
+import Clocks from '../clocks';
 import PostAnything from '../post-anything';
 import BuildStatus from '../build-status';
 
+import styles from './styles.css';
+
 const App = () => (
   <div>
-    <Clock />
-    <PostAnything />
-    <BuildStatus />
+    <Clocks />
+    <div className="row">
+      <div style={{ marginRight: '5px' }}>
+        <BuildStatus />
+      </div>
+      <div>
+        <BuildStatus />
+      </div>
+      <div style={{ marginLeft: '5px' }}>
+        <BuildStatus />
+      </div>
+    </div>
+    <div className="row">
+      <PostAnything />
+    </div>
   </div>
 )
 
